a274f14a96968ce852697f155cee2bbee1697c04
Andrew Pouliot <andrew@darknoon.com>
2011-04-05 12:18:11 -0700

    Added EAGL Extension stuff

---
+++ WoollyMammoth/Engine/Util/EAGLContext+Extensions.h
@@ -0,0 +1,14 @@
+//
+//  EAGLContext+Extensions.h
+//  WoollyMammoth
+//
+//  Created by Andrew Pouliot on 4/5/11.
+//  Copyright 2011 Darknoon. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+
+@interface EAGLContext (EAGLContext_Extensions)
+
+@end
---
+++ WoollyMammoth/Engine/Util/EAGLContext+Extensions.m
@@ -0,0 +1,14 @@
+//
+//  EAGLContext+Extensions.m
+//  WoollyMammoth
+//
+//  Created by Andrew Pouliot on 4/5/11.
+//  Copyright 2011 Darknoon. All rights reserved.
+//
+
+#import "EAGLContext+Extensions.h"
+
+
+@implementation EAGLContext (EAGLContext_Extensions)
+
+@end
